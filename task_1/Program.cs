e448457db814c482269024272652dfb0ced7251b
oldboyee <fuukingy@gmail.com>
2021-12-16 20:56:27 +0200

    dz1

--- task_1/Program.cs
+++ task_1/Program.cs
@@ -9,11 +9,11 @@ namespace task_1
         static void Main(string[] args)
         {
             var classes = new[]
-      {
-       new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
-       new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
-       new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
-     };
+              {
+               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+              };
             var allStudents = GetAllStudents(classes);
 
             Console.WriteLine(string.Join(" ", allStudents));
